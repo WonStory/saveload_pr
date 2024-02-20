83012ab01a7f4e57701861513d9eb8b4f35e7e2e
WonStory <160237395+WonStory@users.noreply.github.com>
2024-02-20 21:39:32 +0900

    20240220
    pr

---
+++ savelodepractice/Assets/DataManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Playerdata
+{
+   public string name;
+   public int level;
+   public int coin;
+   public int item;
+}
+
+
+
+
+public class DataManager : MonoBehaviour
+{
+    //싱글톤
+    public static DataManager instance;
+
+    Playerdata nowplayer = new Playerdata(){name = "박요한", level = 10, coin = 3, item = 3};
+    string Path;
+    string filename = "save";
+
+    private void Awake()
+        {
+            #region 싱글톤
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Destroy(instance.gameObject);
+            }
+            DontDestroyOnLoad(this.gameObject);
+            #endregion
+        
+        
+        Path = Application.persistentDataPath + "/";
+        
+        }
+    /*
+    public void SaveData()
+    {
+        string data = JsonUtility.ToJson(nowplayer);
+        File.WriteAllText(Path + filename,data);
+    }
+    public void LoadData()
+    {
+        File.ReadAllText(Path + filename)
+        JsonUtility.FromJson<Playerdata>(data); //data에 덮어 씌워진다. 이제 파일네임 건드리면 플롯이 달라짐
+
+    }
+*/
+    // Start is called before the first frame update
+    void Start()
+    {
+        string data = JsonUtility.ToJson(nowplayer);
+        File.WriteAllText(Path + filename,data);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
